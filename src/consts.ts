cfb16b751de0527801fc2952d801e90fabdace6c
dding-g <area409@gmail.com>
2024-12-30 01:50:06 +0900

    feat: domain 추가

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = "https://dding-g.github.io";
+export const SITE_URL = "https://www.dding-g.com/";
 export const SITE_TITLE = "dding-glog";
 export const SITE_DESCRIPTION = "dding-g 개발 블로그";
 export const EMAIL = "area409@gmail.com";
