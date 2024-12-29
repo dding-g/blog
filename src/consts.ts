a992bcc1d92fe5bac32d8448e436e024e0d6f225
dding-g <area409@gmail.com>
2024-12-30 01:30:55 +0900

    feat: logo, home 추가

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
-export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
-
-export const EMAIL = "hello@nicholasly.com";
+export const SITE_URL = "https://dding-g.github.io";
+export const SITE_TITLE = "dding-glog";
+export const SITE_DESCRIPTION = "dding-g 개발 블로그";
+export const EMAIL = "area409@gmail.com";
